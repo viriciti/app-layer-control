b97b2bea7070bf650a3a1814d5b869df3529539d
Youssri Krap <y.krap@viriciti.com>
2019-03-22 10:29:08 +0100

    Provide feedback when data has been copied to the clipboard

--- src/client/routes/devices/components/DeviceListItem.js
+++ src/client/routes/devices/components/DeviceListItem.js
@@ -1,96 +1,126 @@
-import React, { PureComponent } from 'react'
-import { connect } from 'react-redux'
-import clipboard from 'clipboard-polyfill'
-import { partialRight } from 'lodash'
+import $ from 'jquery'
+import * as clipboard from 'clipboard-polyfill'
+import React, { useEffect, useRef } from 'react'
 import classNames from 'classnames'
+import { connect } from 'react-redux'
 
 import formats from '/routes/devices/components/table/formats'
 import { selectDevice, multiSelectDevice } from '/routes/devices/actions'
 import toReactKey from '/utils/toReactKey'
 
-const Clipboard = ({ onClick }) => (
-	<button className="btn btn--text btn--icon btn-sm float-right" title="Copy to clipboard" onClick={onClick}>
-		<span className="far fa-clipboard" />
-	</button>
-)
+const Clipboard = value => {
+	const ref    = useRef()
+	const onCopy = event => {
+		const $element = $(ref.current)
 
-class DeviceListItem extends PureComponent {
-	onCopy = (event, value) => {
 		event.stopPropagation()
 		clipboard.writeText(value)
+
+		$element.tooltip('show')
+		setTimeout(() => $element.tooltip('hide'), 1250)
 	}
 
-	onMultiSelect = () => {
-		this.props.multiSelectDevice(this.props.info.get('deviceId'))
-	}
+	useEffect(() => {
+		const $element = $(ref.current)
+		const options  = { trigger: 'manual', html: true }
+		const html     = '<span class="fas fa-clipboard-check fa-fw"></span> Copied'
 
-	onSelectDevice = event => {
+		$element.attr('title', html).tooltip(options)
+		return () => $element.tooltip('dispose')
+	}, [])
+
+	return (
+		<button
+			className="btn btn--text btn--icon btn-sm float-right"
+			onClick={onCopy}
+			ref={ref}
+		>
+			<span className="far fa-clipboard" />
+		</button>
+	)
+}
+
+const DeviceListItem = ({
+	multiSelectDevice,
+	selectDevice,
+	info,
+	deviceSources,
+	selected,
+}) => {
+	const onMultiSelect  = () => multiSelectDevice(info.get('deviceId'))
+	const onSelectDevice = event => {
+		const textContent = event.target.textContent.trim()
 		const selection   = window
 			.getSelection()
 			.toString()
 			.trim()
-		const textContent = event.target.textContent.trim()
 
 		if (!selection || !textContent.includes(selection)) {
-			this.props.selectDevice(this.props.info.get('deviceId'))
+			selectDevice(info.get('deviceId'))
 		}
 	}
+	const stopPropagation = event => event.stopPropagation()
 
-	stopPropagation = event => {
-		event.stopPropagation()
-	}
+	return (
+		<tr
+			className={classNames('device-item', 'tr--cursor', {
+				'table-selected': selected,
+				'table-faded':    !info.has('connected'),
+			})}
+			onClick={onSelectDevice}
+		>
+			<td>
+				<div
+					className="custom-control custom-checkbox"
+					onClick={stopPropagation}
+				>
+					<input
+						className="custom-control-input"
+						type="checkbox"
+						onChange={onMultiSelect}
+						checked={selected}
+						id={`selectDevice${info.get('deviceId')}`}
+					/>
 
-	render () {
-		const { info, deviceSources } = this.props
+					<label
+						className="custom-control-label"
+						htmlFor={`selectDevice${info.get('deviceId')}`}
+					/>
+				</div>
+			</td>
 
-		return (
-			<tr
-				className={classNames('device-item', 'tr--cursor', {
-					'table-selected': this.props.selected,
-					'table-faded':    !this.props.info.has('connected'),
+			{deviceSources
+				.valueSeq()
+				.filter(options => options.get('entryInTable'))
+				.map(options => {
+					const getIn         = options.get('getIn').split('.')
+					const fallbackGetIn = options.get('fallbackGetIn', '').split('.')
+					const getInTitle    = options.get('getInTitle').split('.')
+
+					const value     = info.getIn(
+						getIn,
+						info.getIn(fallbackGetIn, options.get('defaultValue'))
+					)
+					const formatter = formats(options.get('format', 'default'))
+					const span      = formatter({
+						value: value,
+						title: info.getIn(getInTitle),
+						info:  info,
+					})
+
+					return (
+						<td
+							key={toReactKey(info.get('deviceId'), options.get('headerName'))}
+						>
+							{span}
+							{options.get('copyable') && value ? (
+								<Clipboard value={value} />
+							) : null}
+						</td>
+					)
 				})}
-				onClick={this.onSelectDevice}
-			>
-				<td>
-					<div className="custom-control custom-checkbox" onClick={this.stopPropagation}>
-						<input
-							className="custom-control-input"
-							type="checkbox"
-							onChange={this.onMultiSelect}
-							checked={this.props.selected}
-							id={`selectDevice${info.get('deviceId')}`}
-						/>
-
-						<label className="custom-control-label" htmlFor={`selectDevice${info.get('deviceId')}`} />
-					</div>
-				</td>
-
-				{deviceSources
-					.valueSeq()
-					.filter(options => options.get('entryInTable'))
-					.map(options => {
-						const getIn         = options.get('getIn').split('.')
-						const fallbackGetIn = options.get('fallbackGetIn', '').split('.')
-						const getInTitle    = options.get('getInTitle').split('.')
-
-						const value     = info.getIn(getIn, info.getIn(fallbackGetIn, options.get('defaultValue')))
-						const formatter = formats(options.get('format', 'default'))
-						const span      = formatter({
-							value: value,
-							title: info.getIn(getInTitle),
-							info:  info,
-						})
-
-						return (
-							<td key={toReactKey(info.get('deviceId'), options.get('headerName'))}>
-								{span}
-								{options.get('copyable') && value ? <Clipboard onClick={partialRight(this.onCopy, value)} /> : null}
-							</td>
-						)
-					})}
-			</tr>
-		)
-	}
+		</tr>
+	)
 }
 
 export default connect(
