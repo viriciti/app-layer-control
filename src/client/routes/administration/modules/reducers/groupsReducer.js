b26d4f108f734fc67651e531a7a6e3c6a370d35c
Youssri Krap <y.krap@viriciti.com>
2019-06-07 15:26:08 +0200

    Fix group deletion not updating the list of groups

--- src/client/routes/administration/modules/reducers/groupsReducer.js
+++ src/client/routes/administration/modules/reducers/groupsReducer.js
@@ -1,4 +1,5 @@
 import { Map, fromJS } from 'immutable'
+import { difference, map } from 'lodash'
 
 import { GROUPS } from '/routes/administration/modules/actions'
 import createReducer from '/store/createReducer'
@@ -6,9 +7,18 @@ import createReducer from '/store/createReducer'
 export default createReducer(
 	{
 		[GROUPS] (state, action) {
+			if (state.size > action.payload.length) {
+				difference(
+					state.keySeq().toArray(),
+					map(action.payload, 'label')
+				).forEach(name => {
+					state = state.delete(name)
+				})
+			}
+
 			return action.payload.reduce(
 				(groups, group) =>
-					groups.mergeIn([group.label], fromJS(group.applications)),
+					groups.setIn([group.label], fromJS(group.applications)),
 				state
 			)
 		},
